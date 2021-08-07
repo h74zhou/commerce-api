5e09ed168bdaaa595eb49018b974be1663a20811
Herun Zhou <iamherunzhou@gmail.com>
2021-08-07 16:56:20 -0400

    removed the need for user_id for creating cart

--- app/graphql/mutations/create_cart.rb
+++ app/graphql/mutations/create_cart.rb
@@ -1,10 +1,9 @@
 module Mutations
   class CreateCart < BaseMutation
-    argument :user_id, ID, required: false
 
     type Types::CartType
 
-    def resolve(user_id: nil)
+    def resolve
       unless context[:current_user] 
         raise GraphQL::ExecutionError, "You need to sign in to create a Cart"
       end
